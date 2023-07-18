eddefe31491cdbca18d449a9e8e117f4006c9129
GrantLicata <grant.licata@gmail.com>
2023-07-17 21:57:25 -0700

    Navigation bar styled

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -12,7 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="max-w-3xl">{children}</div>
+        <div className="max-w-3xl mx-auto">
+          <Navbar />
+          {children}
+        </div>
       </body>
     </html>
   );
