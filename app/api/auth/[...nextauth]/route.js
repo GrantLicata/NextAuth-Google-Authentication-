c58ea2c2bf589c3b95bff1f07431f583b7376055
GrantLicata <grant.licata@gmail.com>
2023-08-01 21:17:42 -0700

    All functionality complete for google authentication

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,17 +1,53 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/User";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 const authOptions = {
+  // Providers defined for NextAuth processing
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+
+  // User information returned from actions
   callbacks: {
     async signIn({ user, account }) {
-      console.log("User: ", user);
-      console.log("Account: ", account);
+      // If a user signs in with google then store their user data within our database.
+      if (account.provider === "google") {
+        const { name, email } = user;
+        try {
+          await connectMongoDB();
+
+          // Check the database to see if the user already exists
+          const userExists = await User.findOne({ email });
+
+          // If the user doesn't already exist then store them in our database
+          if (!userExists) {
+            const res = await fetch("http://localhost:3000/api/user", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                name,
+                email,
+              }),
+            });
+
+            // Sign in the user if data successfully entered into the database
+            if (res.ok) {
+              return user;
+            }
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      // Returning the user at any point in this process signs in the user
       return user;
     },
   },
