4b389bc9266b492c8b7c0b8cf904bed8ca0f9d02
GrantLicata <grant.licata@gmail.com>
2023-08-17 21:11:18 -0700

    Clerification of function actions

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 const UserInfo = () => {
   const { status, data: session } = useSession();
 
+  // Check if user has been authenticated
   if (status === "authenticated") {
     return (
       <div className="shadow-xl p-8 rounded-md flex flex-col gap-3 bg-yellow-200">
